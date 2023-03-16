7523fdcd98b0653c37057a69f88039d607299ff2
abranllis <47560487+abranllis@users.noreply.github.com>
2023-03-16 17:38:35 +0100

    1

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,21 @@
 import java.util.List;
-//Y por último, podemos crear un objeto Map y utilizar el método findPath para encontrar el camino más corto:
+//Y por último, podemos crear un objeto Map y
+// utilizar el método findPath para encontrar el camino más corto:
 public class Main {
     public static void main(String[] args) {
-        Map map = new Map(10, 10);
-        Node start = map.getNode(0, 0);
-        Node end = map.getNode(9, 9);
+        Map map = new Map(10, 10); // creamos objeto map con numero total de filas y columnas de la matriz
+        Node start = map.getNode(0, 0); // creamos objeto node "inicio" - nodo de inicio
+        Node end = map.getNode(9, 9);   // creamos objeto node "final" - nodo final
 
+        // Creamos una lista de objetos tipo nodo que llamamos path
+        //llamamos al metodo findPath que devuelva la lista de nodos hasta el final
         List<Node> path = map.findPath(start, end);
 
         if (path == null) {
             System.out.println("No se pudo encontrar un camino.");
         } else {
             for (Node node : path) {
-                System.out.println("(" + node.x + ", " + node.y + ")");
+                System.out.println("(" + node.x + ", " + node.y + ")"); //recorremos la lista de nodos y los mostramos
             }
         }
     }
